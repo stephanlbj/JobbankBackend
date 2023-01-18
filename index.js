6792cc81ee87d1ebb1127254a673765d88c29aa6
stephanlbj <stephlbj23@gmail.com>
2023-01-18 15:36:05 +0000

    cors fixed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const Port = process.env.PORT || 5000
 //|
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000','https://jobbankfrontend.vercel.app/','https://jobbankfrontend.vercel.app'],
     
 }));
 
